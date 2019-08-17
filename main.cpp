defbfdc51b4c8c3d5060b3fef003026d648f8411
stevejwebb <stevejwebb@yahoo.com>
2019-08-17 16:59:46 +0100

    t3

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+void cls() noexcept 
+{
+    for(int x=0;x<120;x++) cout << endl;
+}
+void cls(int lines) noexcept
+{
+    for(int x=0;x<lines;x++) cout << endl;
+}
+
+
 int main(int argc, char* argv[]){
+    cls(4);
     array<string, 4> arr {"steve","eve","chloe","reg"};
     cout << arr.size() << endl;
     for (string output : arr)
