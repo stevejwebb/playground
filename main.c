f82cebc3ed917dc00800218bd9fea0619072af35
Steve Webb <stevejwebb@yahoo.com>
2023-11-02 15:38:01 +0000

    sync from mac

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(int argc, char* argv[])
 {
     printf("This is a test of the git commit\n");
     printf("So I can commit this as well!\n");
+    printf("This has been changed from the mac air\n");
 
     return 0;
 }
